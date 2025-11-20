fba75c8244f4e54965a2d7849a058e0b18829628
Sann-Htet <sannhtet899@gmail.com>
2025-11-20 14:02:24 +0630

    first hello world using c lanugage

--- Week1-C/hello.c
+++ Week1-C/hello.c
@@ -1,7 +1,8 @@
-#include <cs50.h>
+// #include <cs50.h>
 #include <stdio.h>
 
 int main(void){
-    string answer = get_string("What's your name? ");
-    printf("hello, %s\n", answer);
+    // string answer = get_string("What's your name? ");
+    // printf("hello, %s\n", answer);
+    printf("hello, world\n");
 }
