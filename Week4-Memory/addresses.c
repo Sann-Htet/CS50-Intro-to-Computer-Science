6c5f6e4f57947cabf34892d3153a84a4b36a32c9
Sann-Htet <sannhtet899@gmail.com>
2025-12-08 08:51:46 +0630

    string pointer

--- Week4-Memory/addresses.c
+++ Week4-Memory/addresses.c
@@ -2,7 +2,10 @@
 
 int main(void)
 {
-    int n = 50;
-    int *p = &n;
-    printf("%p\n", p);
+    char *s = "HI!";
+    printf("%p\n", s);
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
 }
