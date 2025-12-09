3d3efd04f7855d6e1bb6914aad1e3c8c614c0a57
Sann-Htet <sannhtet899@gmail.com>
2025-12-09 08:06:21 +0630

    pointer arithmetic

--- Week4-Memory/addresses.c
+++ Week4-Memory/addresses.c
@@ -3,7 +3,7 @@
 int main(void)
 {
     char *s = "HI!";
-    printf("%c\n", *s);
-    printf("%c\n", *(s+1));
-    printf("%c\n", *(s+2));
+    printf("%s\n", s);
+    printf("%s\n", s+1);
+    printf("%s\n", s+2);
 }
