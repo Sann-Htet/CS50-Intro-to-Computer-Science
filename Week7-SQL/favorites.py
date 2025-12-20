c9ed9c5f4b0c02e9c03d1bde5b979b510342f334
Sann-Htet <sannhtet899@gmail.com>
2025-12-20 08:17:58 +0630

    reading csv file

--- Week7-SQL/favorites.py
+++ Week7-SQL/favorites.py
@@ -2,6 +2,17 @@ import csv
 
 with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
+    counts = dict()
     for row in reader:
-        favorite = row["language"]
-        print(favorite)
+        favorite = row["problem"]
+        if favorite in counts:
+            counts[favorite] += 1
+        else:
+            counts[favorite] = 1
+
+# for favorite in sorted((counts), key=lambda language: counts[language], reverse=True):
+#     print(f"{favorite}: {counts[favorite]}")
+
+favorite = input("Favorite: ")
+if favorite in counts:
+    print(f"{favorite}: {counts[favorite]}")
